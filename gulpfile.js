448c62b7e9c49894d698e89ac23ce167349243f9
Aleksandr Losev <cahektpx1990@gmail.com>
2021-05-12 12:51:49 +0300

    add gulp js changes

--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,7 @@ gulp.task('browser-sync', function() {
 gulp.task('checkupdate', function () {
   gulp.watch('./src/assets/scss/**/*.scss', gulp.parallel('sass'));
   gulp.watch('./src/*.html').on('change', browserSync.reload);
+  gulp.watch('./src/*.js').on('change', browserSync.reload);
 });
 
 gulp.task(
