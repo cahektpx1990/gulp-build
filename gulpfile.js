3b955a25a7787641a54f009a57154a031ccfb598
Aleksandr Losev <cahektpx1990@gmail.com>
2021-05-16 20:21:38 +0300

    added gulp-include

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ const rename = require("gulp-rename");
 const gcmq = require('gulp-group-css-media-queries');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
-sass.compiler = require('node-sass');
+const include = require('gulp-include');
+// sass.compiler = require('node-sass');
 
 
 // Срабатывает все тут
@@ -15,10 +16,7 @@ gulp.task('sass', function () {
   return gulp.src('./src/assets/scss/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    // .pipe(gulp.dest('./src/assets/css/dist'));
-    .pipe(autoprefixer({
-      cascade: false
-    }))
+    .pipe(autoprefixer())
     .pipe(gcmq())
     .pipe(csso())
     .pipe(rename({
@@ -27,6 +25,16 @@ gulp.task('sass', function () {
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest("./src/assets/css"))
     .pipe(browserSync.reload({ stream: true}));    
+});
+
+gulp.task("js", function () {
+  return gulp.src('./src/assets/js/index.js')
+    .pipe(include())
+      .on('error', console.log)
+      // 
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./src/assets/js'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('browser-sync', function() {
@@ -43,13 +51,7 @@ gulp.task('browser-sync', function() {
 gulp.task('checkupdate', function () {
   gulp.watch('./src/assets/scss/**/*.scss', gulp.parallel('sass'));
   gulp.watch('./src/*.html').on('change', browserSync.reload);
-  gulp.watch('./src/*.js').on('change', browserSync.reload);
+  gulp.watch('./src/assets/js/components/**/*.js').on('change', browserSync.reload);
 });
 
-gulp.task(
-  "watch",
-  gulp.parallel(
-    'sass',
-    'checkupdate'
-  )
-);
+gulp.task("watch", gulp.parallel("sass", "js", "checkupdate", "browser-sync"));
